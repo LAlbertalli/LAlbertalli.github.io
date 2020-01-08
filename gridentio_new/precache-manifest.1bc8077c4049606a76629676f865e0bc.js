24866ace9062c4dfa6c557aaee05060462595119
luca <l.albertalli@gmail.com>
2020-01-08 16:00:34 +0100

    First push of Gridentio Modified

---
+++ gridentio_new/precache-manifest.1bc8077c4049606a76629676f865e0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba2edcead8442765b08aff94c3ef973",
+    "url": "/gridentio_new/index.html"
+  },
+  {
+    "revision": "333c23f96d2b93c10b20",
+    "url": "/gridentio_new/static/css/main.79e85b90.chunk.css"
+  },
+  {
+    "revision": "4c96b355ce8525f1a240",
+    "url": "/gridentio_new/static/js/2.5a8df08e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gridentio_new/static/js/2.5a8df08e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "333c23f96d2b93c10b20",
+    "url": "/gridentio_new/static/js/main.4ec00883.chunk.js"
+  },
+  {
+    "revision": "9af02cdc28eb4fe9f8a4",
+    "url": "/gridentio_new/static/js/runtime-main.376c419a.js"
+  }
+]);
